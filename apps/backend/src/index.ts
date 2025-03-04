0b4783a73d389aabdc5a27ed17f9d73bd29ce0f2
yamazaki-yuki-23 <natural4083@gmail.com>
2025-03-05 01:35:46 +0900

    バックエンドにcors設定を追加

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,9 +1,14 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
 
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.use('*', cors({
+  origin: '*',
+}))
+const route = app.get('/hello', (c) => {
+  return c.json({ message: 'Hello Hono!' })
 })
+export type AppType = typeof route
 
 export default app
