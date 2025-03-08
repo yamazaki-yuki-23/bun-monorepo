cd362f1bc504ec94e544f2ddb216ed9f233f0c15
yamazaki-yuki-23 <natural4083@gmail.com>
2025-03-08 14:57:34 +0900

    zod install

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,14 +1,32 @@
+import { zValidator } from '@hono/zod-validator'
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
+import { z } from 'zod'
 
 const app = new Hono()
 
 app.use('*', cors({
   origin: '*',
 }))
-const route = app.get('/hello', (c) => {
-  return c.json({ message: 'Hello Hono!' })
+
+const todoSchema = z.object({
+  title: z.string().min(2),
+  description: z.string().nullable(),
 })
+
+const route = app
+  .get('/hello', (c) => {
+    return c.json({ message: 'Hello Hono!' })
+  })
+  .post('/todo', zValidator('json', todoSchema, (result, c) => {
+    if (!result.success) {
+      return c.text(result.error.issues[0].message, 400)
+    }
+  }),
+    (c) => {
+      const { title, description } = c.req.valid('json')
+      return c.json({ title, description })
+    })
 export type AppType = typeof route
 
 export default app
--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,16 +1,34 @@
 'use client'
 
 import { client } from "@/utils/client";
+import { useActionState } from "react";
 
 export default function Home() {
-  const handleClick = async () => {
-    const res = await client.hello.$get()
-    const data = await res.json()
-    alert(data.message)
+  const formAction = async (prevError: string | null, formData: FormData) => {
+    const title = formData.get('title') as string
+    const description = formData.get('description') as string
+    const res = await client.todo.$post({
+      json: { title, description },
+    })
+    if (!res.ok) {
+      const error = await res.text()
+      return error
+    }
+    return null
   }
+  const [error, submitAction, isPending] = useActionState(formAction, null)
+
   return (
-    <div>
-      <button onClick={handleClick}>Click me</button>
+    <div className="mt-10">
+      <h1 className="text-3xl font-bold text-center">Todo</h1>
+      <form action={submitAction} className="flex flex-col gap-2 max-w-[600px] mx-auto mt-10">
+        <label htmlFor="title" className="text-sm font-medium">Title</label>
+        <input type="text" name="title" className="border-2 border-gray-300 rounded-md p-2" />
+        <label htmlFor="description" className="text-sm font-medium">Description</label>
+        <input type="text" name="description" className="border-2 border-gray-300 rounded-md p-2" />
+        <button disabled={isPending} type="submit" className="bg-blue-500 text-white p-2 rounded-md">Submit</button>
+        {error && <p className="text-red-500">{error}</p>}
+      </form>
     </div>
   );
 }
