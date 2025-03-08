065e6db11c5996c175e694eb95b2e1e6de37ca2c
yamazaki-yuki-23 <natural4083@gmail.com>
2025-03-08 15:22:38 +0900

    install supabase, drizzle

---
+++ apps/backend/src/db/schema.ts
@@ -0,0 +1,8 @@
+import { integer, pgTable, varchar, timestamp } from "drizzle-orm/pg-core";
+
+export const todoTable = pgTable("todos", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  title: varchar({ length: 255 }).notNull(),
+  description: varchar({ length: 255 }),
+  createdAt: timestamp().notNull().defaultNow(),
+});
